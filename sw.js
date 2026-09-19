0f256604d24a4b021ebba367ebc656dc278be4f4
ankushchhajed7-cmd <ankushchhajed7@gmail.com>
2026-09-19 21:50:06 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE = 'apk-factory-v1';
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./', './index.html'])).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys()
+    .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+    .then(() => self.clients.claim()));
+});
+
+self.addEventListener('fetch', e => {
+  const req = e.request;
+  if (req.method !== 'GET') return;
+  if (new URL(req.url).origin !== location.origin) return;
+  e.respondWith(
+    fetch(req)
+      .then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(req, copy));
+        return res;
+      })
+      .catch(() => caches.match(req).then(r => r || caches.match('./index.html')))
+  );
+});
